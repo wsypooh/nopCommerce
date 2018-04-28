b011c7080f9dc2da65e360d8842e3c9c0de2f298
DmitriyKulagin <kulagin87@gmail.com>
2018-04-28 11:59:20 +0300

    removed Filter from QueryableExtensions

--- src/Presentation/Nop.Web.Framework/Kendoui/QueryableExtensions.cs
+++ src/Presentation/Nop.Web.Framework/Kendoui/QueryableExtensions.cs
@@ -10,33 +10,6 @@ namespace Nop.Web.Framework.Kendoui
     public static class QueryableExtensions
     {
         /// <summary>
-        /// Filter a collection
-        /// </summary>
-        /// <typeparam name="T">Type</typeparam>
-        /// <param name="queryable">Collection</param>
-        /// <param name="filter">Filter parameters</param>
-        /// <returns>Result</returns>
-        public static IQueryable<T> Filter<T>(this IQueryable<T> queryable, Filter filter)
-        {
-            if (filter != null && filter.Logic != null)
-            {
-                // Collect a flat list of all filters
-                var filters = filter.All();
-
-                // Get all filter values as array (needed by the Where method of Dynamic Linq)
-                var values = filters.Select(f => f.Value).ToArray();
-
-                // Create a predicate expression e.g. Field1 = @0 And Field2 > @1
-                var predicate = filter.ToExpression(filters);
-
-                // Use the Where method of Dynamic Linq to filter the data
-                queryable = queryable.Where(predicate, values);
-            }
-
-            return queryable;
-        }
-
-        /// <summary>
         /// Sort a collection
         /// </summary>
         /// <typeparam name="T">Type</typeparam>
